9075877ebdb13e67ac3eba6a408f7b17745f17a2
wanaka <asd8116@gmail.com>
2019-09-08 21:29:56 +0800

    Install packages & init files

--- src/main.js
+++ src/main.js
@@ -2,7 +2,40 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import '../src/assets/index.css'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+import { faServer, faSearch, faCog, faFolderPlus, faFileUpload, faEllipsisH, faPlus, faChevronRight, faTimes, faCaretDown } from '@fortawesome/free-solid-svg-icons'
+
+import { faFolder, faFileWord, faFilePowerpoint, faFilePdf, faFileImage, faFileExcel, faClock, faStar, faTrashAlt, faBell, faUser } from '@fortawesome/free-regular-svg-icons'
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(
+  faFolder,
+  faFileWord,
+  faFilePowerpoint,
+  faFilePdf,
+  faFileImage,
+  faFileExcel,
+  faClock,
+  faStar,
+  faTrashAlt,
+  faServer,
+  faBell,
+  faSearch,
+  faCog,
+  faFolderPlus,
+  faFileUpload,
+  faEllipsisH,
+  faUser,
+  faPlus,
+  faChevronRight,
+  faTimes,
+  faCaretDown
+)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.config.productionTip = false
 
 new Vue({
--- src/store.js
+++ src/store.js
@@ -1,16 +1,77 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { type } from 'os'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    fileList: [
+      {
+        name: ' 新增資料夾',
+        type: 'folder',
+        owner: ' 我',
+        changeTime: ' 剛剛',
+        size: 2000000 //KB
+      },
+      {
+        name: 'Hello World.docx',
+        type: 'docx',
+        owner: ' 我',
+        changeTime: ' 剛剛',
+        size: 540 //KB
+      },
+      {
+        name: 'Hello World.jpg',
+        type: 'img',
+        owner: ' 我',
+        changeTime: '1 小時前',
+        size: 2000 //KB
+      },
+      {
+        name: 'Hello World.xlsx',
+        type: 'xlsx',
+        owner: ' 我',
+        changeTime: '1 天前',
+        size: 1200 //KB
+      },
+      {
+        name: 'Hello World.pdf',
+        type: 'pdf',
+        owner: ' 我',
+        changeTime: '2019 年 10 月 31 日',
+        size: 10000 //KB
+      },
+      {
+        name: 'Hello World.docx',
+        type: 'docx',
+        owner: ' 我',
+        changeTime: ' 剛剛',
+        size: 540 //KB
+      },
+      {
+        name: 'Hello World.jpg',
+        type: 'img',
+        owner: ' 我',
+        changeTime: '1 小時前',
+        size: 2000 //KB
+      },
+      {
+        name: 'Hello World.xlsx',
+        type: 'xlsx',
+        owner: ' 我',
+        changeTime: '1 天前',
+        size: 1200 //KB
+      },
+      {
+        name: 'Hello World.pdf',
+        type: 'pdf',
+        owner: ' 我',
+        changeTime: '2019 年 10 月 31 日',
+        size: 10000 //KB
+      }
+    ]
   },
-  mutations: {
-
-  },
-  actions: {
-
-  }
+  mutations: {},
+  actions: {}
 })
